75195eea97afcf06efb86bae52a8e3ab33ef601d
yuki-on <lipekunnn@gmail.com>
2023-04-06 16:19:52 +0000

    ocean: Set TARGET_BOOT_ANIMATION_RES

--- lineage_ocean.mk
+++ lineage_ocean.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device specific flags
+TARGET_BOOT_ANIMATION_RES := 720
+
 # Inherit from ocean device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
